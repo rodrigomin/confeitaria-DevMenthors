b7168c3f86da43f11fdf163a94ea8a63533fe049
Rodrigo Minoti Costa <rodrigo.minotic@gmail.com>
2025-09-27 19:12:02 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("pipi");
+  
+  function mapRange(value, inMin, inMax, outMin, outMax) { // chat ajudou
+    return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
+  }
+  
+  
+window.onload = () => {
+    const ofertaScroll = document.getElementById('scroll-ofertas')
+    const ofertasContainers = document.querySelectorAll('div.ofertas-container')
+    ofertasContainers[0].style.scale = 1.05 // p n bugar
+
+    ofertaScroll.addEventListener("scroll", () => {
+        let scrollValue = ofertaScroll.scrollLeft
+
+        let maxScroll = ofertaScroll.scrollWidth
+        console.log(maxScroll)
+
+        if (scrollValue <= 150) {
+          let mapped = mapRange(scrollValue, 0, 125+(240), 1.075, 1)
+          ofertasContainers[0].style.scale = mapped
+        }
+      
+        if (scrollValue > 175 && scrollValue <= 425) {
+          let mapped = mapRange(scrollValue, 175, 125+(240), 1, 1.075)
+          if (scrollValue > 125+(240)) // olha o reverse uoow
+            mapped = mapRange(scrollValue, 125+(240), 125+(240*2), 1.075, 1)
+          ofertasContainers[1].style.scale = mapped
+        }
+      
+        if (scrollValue > 425) {
+          let maxScroll = ofertaScroll.scrollWidth - ofertaScroll.clientWidth
+          let mapped = mapRange(scrollValue, 125+(240*2), 125+(240*3), 1, 1.075)
+          ofertasContainers[2].style.scale = mapped
+        }
+      })
+    
+}
+
+
+
+// 150 ; 425 ; 556
