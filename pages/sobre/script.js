4198bcacaf85287fc883f1a9fc6b62532d6c2711
LoboNoob <rochageovane05@gmail.com>
2025-10-11 11:56:45 -0300

    page Sobre

--- pages/sobre/script.js
+++ pages/sobre/script.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 const menuIcon = document.getElementById("menu-icon");
 const menu = document.getElementById("menu");
 
 menuIcon.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
-=======
-const menuIcon = document.getElementById("menu-icon");
-const menu = document.getElementById("menu");
-
-menuIcon.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
->>>>>>> f6acf9e3fcf2d718665eceb2ec3e0fabd6de5037
